3b9923f695ef8fef8ae9218ae800e5c302566e41
parapente-dans-le-nord <parapente-dans-le-nord@protonmail.me>
2024-01-19 15:45:57 +0100

    yo

--- script.py
+++ script.py
@@ -5,17 +5,20 @@ import random
 repoPath = os.environ['GITHUB_WORKSPACE']
 stepOutputPath = os.environ['GITHUB_OUTPUT']
 
-try:
-    with open(f"{repoPath}/src/spots.json", 'r') as file:
-        data = json.load(file)
-except FileNotFoundError:
-    print(f"file {file} not found.")
-except json.JSONDecodeError:
-    print(f"json not valid in {file}.")
+for var in os.environ:
+    print(var)
 
-spotName = data['spots'][random.randint(0, len(data['spots']))]['name']
-print(f"spotName is {spotName}")
-
-with open(stepOutputPath, 'a') as file:
-    file.write(f"spotName={spotName}")
-
+#try:
+#    with open(f"{repoPath}/src/spots.json", 'r') as file:
+#        data = json.load(file)
+#except FileNotFoundError:
+#    print(f"file {file} not found.")
+#except json.JSONDecodeError:
+#    print(f"json not valid in {file}.")
+#
+#spotName = data['spots'][random.randint(0, len(data['spots']))]['name']
+#print(f"spotName is {spotName}")
+#
+#with open(stepOutputPath, 'a') as file:
+#    file.write(f"spotName={spotName}")
+#
