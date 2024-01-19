674f8af169f3752436663f20575c648fc0315897
parapente-dans-le-nord <parapente-dans-le-nord@protonmail.me>
2024-01-19 17:45:18 +0100

    yo

--- script.py
+++ script.py
@@ -4,21 +4,29 @@ import random
 
 repoPath = os.environ['GITHUB_WORKSPACE']
 stepOutputPath = os.environ['GITHUB_OUTPUT']
+issueBody = os.environ['issueBody']
+
+print("Le body de l'issue")
+print(issueBody)
 
 for var in os.environ:
     print(var)
 
-#try:
-#    with open(f"{repoPath}/src/spots.json", 'r') as file:
-#        data = json.load(file)
-#except FileNotFoundError:
-#    print(f"file {file} not found.")
-#except json.JSONDecodeError:
-#    print(f"json not valid in {file}.")
-#
-#spotName = data['spots'][random.randint(0, len(data['spots']))]['name']
-#print(f"spotName is {spotName}")
-#
-#with open(stepOutputPath, 'a') as file:
-#    file.write(f"spotName={spotName}")
-#
+try:
+    with open(f"{repoPath}/src/spots.json", 'r') as file:
+        data = json.load(file)
+except FileNotFoundError:
+    print(f"file {file} not found.")
+except json.JSONDecodeError:
+    print(f"json not valid in {file}.")
+
+spotName = data['spots'][random.randint(0, len(data['spots']))]['name']
+print(f"spotName is {spotName}")
+
+# Parse body and build json
+# Is spot already in list based on name ?
+#spot already exist = exit 1 with message
+
+with open(stepOutputPath, 'a') as file:
+    file.write(f"spotName={spotName}")
+
